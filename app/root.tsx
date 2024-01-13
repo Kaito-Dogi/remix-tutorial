15a90725f994c718d2d2a8f13fcf2b2df705fcde
Kaito-Dogi <kendobu0405@gmail.com>
2024-01-13 16:13:29 +0900

    style: import を修正

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
-import appStylesHref from "./app.css";
+import appStylesHref from "~/app.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
